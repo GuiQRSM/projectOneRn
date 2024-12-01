e6062c4e8cb99acf361fffce07cfa045cc5b5696
GuiQRSM <quaresmagui20@gmail.com>
2024-12-01 17:37:16 -0300

    Set the JSX in the project

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+ return (
+    <View>
+    <Text>React Native</Text>
+    <Text>React Native</Text>
+    </View>
+ )
+}
