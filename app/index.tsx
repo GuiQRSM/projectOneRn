d84559d9d913132bb899ece9ba2a4d8b40f1317c
GuiQRSM <quaresmagui20@gmail.com>
2024-12-02 16:08:30 -0300

    implements styling elements

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,27 @@ import { Text, View } from 'react-native';
 
 export default function App() {
  return (
-    <View>
-    <Text>React Native</Text>
-    <Text>React Native</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24,
+    }}>
+
+    <Text style={{
+      color: '#FDFCFE',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 48,
+    }}>
+      Nome do evento
+   </Text>
+   
+    <Text style={{
+      color: '#6B6B6B',
+      fontSize: 16,
+      fontWeight: 'bold',
+    }}>Sexta, 4 de Novembro de 2024</Text>
+
     </View>
  )
 }
