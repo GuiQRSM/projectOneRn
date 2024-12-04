03cf07220da3992eb498bd63b9f3d55ad26242d7
GuiQRSM <quaresmagui20@gmail.com>
2024-12-04 16:25:14 -0300

    Organizes the project file structure

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { styles } from './styles'
+
+export default function Home() {
+ return (
+
+    <View style={styles.container}>
+
+    <Text style={styles.eventName}>
+      Nome do evento
+   </Text>
+   
+    <Text style={styles.eventDate}>
+      Sexta, 4 de Novembro de 2024
+    </Text>
+
+    </View>
+ )
+}
