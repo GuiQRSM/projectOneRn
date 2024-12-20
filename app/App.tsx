778351ae8b129fc4047329d0b3fc949f4a7f1e64
GuiQRSM <quaresmagui20@gmail.com>
2024-12-20 20:52:49 -0300

    Set a style to StatusBar

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,17 @@
 import Home from "@/src/screens/Home";
+import React from "react";
+import { StatusBar } from "react-native";
 
 export default function App() {
  return (
-  <Home />
+    <>
+    <StatusBar 
+    barStyle='dark-content'
+    backgroundColor="transparent"
+    translucent={true}
+    />
+     <Home />
+    </>
  );
 }
 
