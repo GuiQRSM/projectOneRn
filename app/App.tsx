3c05c94804d4111ab0be5a8e14dbf24713714653
GuiQRSM <quaresmagui20@gmail.com>
2024-12-20 20:59:07 -0300

    Finishes the StatusBar

--- app/App.tsx
+++ app/App.tsx
@@ -5,12 +5,12 @@ import { StatusBar } from "react-native";
 export default function App() {
  return (
     <>
-    <StatusBar 
+    <StatusBar
     barStyle='dark-content'
-    backgroundColor="transparent"
+    backgroundColor='transparent'
     translucent={true}
     />
-     <Home />
+    <Home />
     </>
  );
 }
